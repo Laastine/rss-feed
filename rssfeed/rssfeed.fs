fec666ed374b3559b3cdba71bb9544002ec67336
Mikko Kaistinen <mikko.kaistinen@reaktor.com>
2016-08-29 21:25:10 +0300

    Fetch rss xml and print it out

--- rssfeed/rssfeed.fs
+++ rssfeed/rssfeed.fs
@@ -1,9 +1,34 @@
 module rssfeed
 
-open Suave
+open System.IO
+open System.Net
+open System.Text.RegularExpressions
+
+let fetch (url: string) =
+  try
+    let req = WebRequest.Create(url) :?> HttpWebRequest
+    req.UserAgent <- "Mozilla/5.0 (Windows NT 6.1; WOW64; Trident/7.0; rv:11.0) like Gecko"
+    req.Timeout <- 3000
+    use response = req.GetResponse()
+    let content = response.ContentType
+    let isXML = Regex("xml").IsMatch(content)
+    match isXML with
+    | true -> use stream = response.GetResponseStream()
+              use reader = new StreamReader(stream)
+              let xml = reader.ReadToEnd()
+              Some xml
+    | false -> None
+  with
+  | _ -> None
+
+let exists (x : string option) =
+    match x with
+    | Some(x) -> printf "%s\n" x
+    | None -> printf "Not found"
 
 [<EntryPoint>]
 let main argv =
     printfn "Starting Suave server on port 8083"
-    startWebServer defaultConfig (Successful.OK "Hello World!")
-    0 // return an integer exit code
+    let xml = fetch "http://feeds.feedburner.com/RockPaperShotgun"
+    exists(xml)
+    0
