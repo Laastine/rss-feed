2b556c6c9862979a991d3449344d75597273e402
Mikko Kaistinen <mikko.kaistinen@kapsi.fi>
2016-09-04 10:06:52 +0300

    'Dynamic' db address loading

--- rssfeed/db.fs
+++ rssfeed/db.fs
@@ -1,23 +1,26 @@
 module db
 
 open System
+open FSharp.Configuration
 open FSharp.Data.Sql
 
 let getEnvVar (envVar: string) = 
   let d = System.Environment.GetEnvironmentVariables()
   d.[envVar] :?> string
 
-let addr = "Server=" + getEnvVar "DATABASE_URL" + ";Database=postgres;User Id=postgres;Password=postgres"
+type Settings = AppSettings<"app.config">
+
+let configConnectionString = Settings.ConnectionStrings.Localhost
 
 type Sql = SqlDataProvider<
               ConnectionString        = "Server=192.168.99.100;Database=rssfeed;User Id=postgres;Password=postgres",
               DatabaseVendor          = Common.DatabaseProviderTypes.POSTGRESQL,
               CaseSensitivityChange   = Common.CaseSensitivityChange.ORIGINAL >
 
-type DbContext = Sql.dataContext
+type DbContext = Sql.dataContext 
 type Feed = DbContext.``public.feedsEntity``
 
-let getContext() = Sql.GetDataContext()
+let getContext() = Sql.GetDataContext configConnectionString
 
 let getFeeds (ctx : DbContext) : Feed list = 
     ctx.Public.Feeds |> Seq.toList
