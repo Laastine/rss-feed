ea273add07d70c246f18b73cbc71bcad39557986
Mikko Kaistinen <mikko.kaistinen@kapsi.fi>
2016-10-23 19:05:52 +0300

    Generate more unique list element keys

--- rssfeed/web/src/pages/partials/feeds.js
+++ rssfeed/web/src/pages/partials/feeds.js
@@ -8,8 +8,8 @@ const Feeds = React.createClass({
 
   render() {
     const singleFeed = propOr([], 'singleFeed')(this.context.appState)
-    return <ul>{singleFeed.map((f) =>
-      <li key={f.title}><a href={f.link}>{f.title}</a>
+    return <ul>{singleFeed.map((f, i) =>
+      <li key={f.title+i}><a href={f.link}>{f.title}</a>
         <div className="feed-element">{f.description}</div>
       </li>
       )}</ul>
